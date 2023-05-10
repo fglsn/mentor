a404c7f352fba237115076a883e66aee56cadfbc
Ilona Shakurova <leverseau19@gmail.com>
2023-05-10 13:32:16 +0300

    Structure directory

---
+++ qr-component/src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './App.css';
+import Card from '../Card';
+
+function App() {
+	return (
+		<div className="App-wrapper">
+			<Card/>
+		</div>
+	);
+}
+
+export default App;
